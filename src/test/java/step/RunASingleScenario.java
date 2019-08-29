0c54147303933f272e08f67bf696d4b4bff9cd99
admin <gnatyshko.i@gmail.com>
2019-08-29 17:39:02 +0200

    lesson#3

---
+++ src/test/java/step/RunASingleScenario.java
@@ -0,0 +1,9 @@
+package step;
+
+import net.serenitybdd.jbehave.SerenityStories;
+import net.serenitybdd.jbehave.annotations.Metafilter;
+
+@Metafilter("login")
+public class RunASingleScenario extends SerenityStories {
+
+}
