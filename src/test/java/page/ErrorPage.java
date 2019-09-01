9ec9d23566556158fc34cc1eeb3e542e0546ab70
admin <gnatyshko.i@gmail.com>
2019-09-01 11:41:51 +0200

    hw#3

---
+++ src/test/java/page/ErrorPage.java
@@ -0,0 +1,46 @@
+package page;
+
+import net.serenitybdd.core.pages.PageObject;
+import net.thucydides.core.annotations.At;
+import net.thucydides.core.annotations.WhenPageOpens;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import static net.thucydides.core.webdriver.ThucydidesWebDriverSupport.getPages;
+
+@At("https://www.linkedin.com/checkpoint/lg/login-submit")
+
+public class ErrorPage extends PageObject {
+
+    public ErrorPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(xpath = "//input[@id='username']")
+    private WebElement userEmailField;
+
+    @FindBy(xpath = "//input[@id='password']")
+    private WebElement userPasswordField;
+
+    @FindBy(xpath = "//button[@type='submit']")
+    private WebElement signInButton;
+
+    @WhenPageOpens
+    public void waitUntilTitleAppears() {
+        element(userEmailField).waitUntilVisible();
+    }
+
+    public void login(String userEmail, String userPassword) {
+        userEmailField.sendKeys(userEmail);
+        userPasswordField.sendKeys(userPassword);
+        signInButton.click();
+    }
+
+    public boolean isPageLoaded() {
+        return getPages().isCurrentPageAt(ErrorPage.class);
+    }
+
+}
+
+
